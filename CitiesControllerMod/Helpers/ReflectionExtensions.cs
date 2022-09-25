a944921e354f39a8f1eecef5ecca6682a8f522b0
cherry <gb.fouronetwo@gmail.com>
2022-09-25 03:21:20 +0200

    invokemethod added to reflectionextensions

--- CitiesControllerMod/Helpers/ReflectionExtensions.cs
+++ CitiesControllerMod/Helpers/ReflectionExtensions.cs
@@ -10,5 +10,17 @@ namespace CitiesControllerMod.Helpers
             var field = obj.GetType().GetField(name, bindingFlags);
             return (T)field?.GetValue(obj);
         }
+
+        public static void InvokeMethod(this object obj, string name)
+        {
+            MethodInfo mi = obj.GetType().GetMethod(name);
+            mi.Invoke(obj, null);
+        }
+
+        public static void InvokePrivateMethod(this object obj, string name)
+        {
+            MethodInfo mi = obj.GetType().GetMethod(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            mi.Invoke(obj, null);
+        }
     }
 }
